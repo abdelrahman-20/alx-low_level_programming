49262f6c6f2df4bdc2db8386b8aaf6c5378cab4c
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-15 19:49:12 -0700

    Advanced Task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Print A Quote
+ *
+ * Return: 1 is Error
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
