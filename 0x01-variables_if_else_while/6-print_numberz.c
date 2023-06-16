81ee343fdb737a14de8783ee37a6b5e0a08ae746
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-16 13:47:39 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - 123
+ *
+ * Description: 123
+ *
+ * Return: 0 is Success
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
