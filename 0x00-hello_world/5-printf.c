b138440b5b4e06de277487a895966f7e761bfdba
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-15 19:11:35 -0700

    Task 05

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/** 
+ * main - This is Entry Function of The Program
+ *
+ * Description: C Program
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
