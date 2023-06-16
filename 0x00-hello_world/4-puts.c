ef26feb81eb5536450a21b4c9b9c0aa0ff156531
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-15 17:55:30 -0700

    Task 04 fix 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+
+/*Main Function of The Program*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
