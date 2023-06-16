79117ef23c34de01b93008204842ef919e3a2f09
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-15 18:04:16 -0700

    Task 04 fix 01

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
-/*Main Function of The Program*/
+/**
+*main - This is Entry Function of The Program
+*return: if (0) it's Success other with it's a failure
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
