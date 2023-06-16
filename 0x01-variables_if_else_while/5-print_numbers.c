8aabcb42fcb65bdb80806fe658063a3a7b62d8f3
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-16 13:40:07 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - 123
+ *
+ * Description: 123
+ *
+ * Return: 123
+*/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		printf("%d", x);
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
