a989b49db2fca63ba2eee1f87f60c45144dfdd0b
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-16 12:38:06 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - abc
+ *
+ * Description: abc
+ *
+ * Return: 0 means success
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
