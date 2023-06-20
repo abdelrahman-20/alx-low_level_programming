220ab5c02462bbbb53e2170d72731f6f9ba5dfa6
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-20 09:24:18 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point of The Program
+ *
+ * Description: Printing
+ *
+ * Return: 0 is Success
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
