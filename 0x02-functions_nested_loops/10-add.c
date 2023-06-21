9fcc6325a4eaea5a55216ab5765487061aa00820
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-20 17:47:34 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - sum func
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the sum of 2 numbers
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
