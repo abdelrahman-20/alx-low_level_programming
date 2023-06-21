9d4af2cfded152b9448920dd6e78fac889a866cd
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-20 18:00:54 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_to_98 - entry function
+ * @n: start number
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("98\n");
+	}
+	else
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("98\n");
+	}
+}
