a349081f53704e53a913ebeabb4708c8a598733b
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-20 18:40:28 -0700

    Task 101 Fix 1

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	int n, sum = 0;
+	int n, sum;
 
 	for (n = 1; n < 1024; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 			sum += n;
 	}
-	printf("%d \n", sum);
+	printf("%d", sum);
 
 	return (0);
 }
