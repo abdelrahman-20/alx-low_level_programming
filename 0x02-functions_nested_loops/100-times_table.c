811cae52d8577ee42408246228acf36f79df27f9
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-20 18:18:28 -0700

    Task 100 Fix 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,16 +8,31 @@
 
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, product;
 
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			if (j != n)
-				printf("%d, ", i * j);
+			product = i * j;
+
+			if (j == 0)
+			{
+				printf("%d, ", product);
+			}
+			else if (j == n)
+			{
+				printf("%d\n", product);
+			}
 			else
-				printf("%d\n", i * j);
+			{
+				if (product <= 9)
+					printf("  %d, ", product);
+				else if (product <= 99)
+					printf(" %d, ", product);
+				else
+					printf("%d, ", product);
+			}
 		}
 	}
 }
