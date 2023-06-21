025561cc0f66b93de759784768bf96e944fa09e7
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-20 17:39:50 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - Entry Func
+*/
+
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(48);
+		for (j = 1; j < 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = i * j;
+
+			if (product <= 9)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + 48);
+			_putchar((product % 10) + 48);
+
+		}
+		_putchar('\n');
+	}
+}
