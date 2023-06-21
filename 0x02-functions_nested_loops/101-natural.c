1e2bfd8d55f28e71ba908c8aa953690c8c188808
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-20 18:37:15 -0700

    Task 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry Function
+ *
+ * Return: 0 is Success
+*/
+
+int main(void)
+{
+	int n, sum = 0;
+
+	for (n = 1; n < 1024; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+			sum += n;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
