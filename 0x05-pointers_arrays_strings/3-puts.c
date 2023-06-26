00eed77415ad8785b5da6cd3808b6e40272c83ce
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-26 16:27:25 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - Print A String
+ * @str: The String To Print
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + '0');
+		*str++;
+	}
+	_putchar('\n');
+}
