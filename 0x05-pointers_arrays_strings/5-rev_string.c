3e8a1905ecf6bbbf20cb7156e60f7aa5efe5fe74
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-26 16:46:10 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - A Function To Reverse A String
+ * @s: The String Parameter
+*/
+
+void rev_string(char *s)
+{
+	int size = 0;
+
+	while (s[size] != '\0')
+		size++;
+
+	for (i = 0; i < (size / 2); i++)
+	{
+		char tmp = s[i];
+
+		s[i] = s[size - i - 1];
+		s[size - i - 1] = tmp;
+	}
+}
