ab6492438de361aef2b7ed59994c92587a644646
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-26 17:28:22 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_array - Function
+ * @a: The Array
+ * @n: The NO. Elements
+*/
+
+void print_array(int *a, int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d\n", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,6 +12,7 @@ void print_rev(char *s);
 void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 
 
 #endif
