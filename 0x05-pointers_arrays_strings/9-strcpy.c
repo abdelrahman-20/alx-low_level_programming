edb23ac04a536fdfb3c237cb545320bdc2645b39
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-26 17:39:52 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - Function
+ * @dest: Distination Var
+ * @src: Source Var
+ * Return: The Dist Var
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	do {
+		dest[i] = src[i];
+		i++;
+	} while (dest[i] != '\0')
+
+	return (dest);
+}
