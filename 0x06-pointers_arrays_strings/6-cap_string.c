9531452b3bbebc46a8f67eaac7c3cc8dc43f2eb4
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-06-29 18:25:19 -0700

    Task 6 Fix 0

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * separator - Check for Delimeter
+ * @c: The Char To Check
+ * Return: 0 or 1
+*/
+
+int separator(char c)
+{
+	int i;
+	char delim[] = " \t\n,.!?\"{}()";
+
+	for (i = 0; i < 12; i++)
+	{
+		if (c == delim[i])
+			return (1);
+	}
+	return (0);
+}
+
+
+/**
  * cap_string - Capitalize Each Word
  * @str: The String
  * Return: The Final String
@@ -12,7 +32,7 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i - 1] == ' ' || i == 0)
+		if (separator(str[i]) || i == 0)
 		{
 			if (str[i] >= 97 && str[i] <= 122)
 				str[i] = str[i] - 32;
