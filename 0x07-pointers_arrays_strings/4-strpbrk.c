4ae16821fa3a1d1f3a2ec2572ecfa233bf22c340
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-03 10:24:38 -0700

    Task 4 Fix 1

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,11 +9,13 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0, j = 0;
+	int i, j;
 	char *p;
 
+	i = 0;
 	while (s[i] != '\0')
 	{
+		j = 0;
 		while (accept[j] != '\0')
 		{
 			if (accept[j] == s[i])
