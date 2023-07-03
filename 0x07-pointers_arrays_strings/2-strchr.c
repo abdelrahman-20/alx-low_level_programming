aa4b58e7d3a5bcc0f30ef31b2daf6769cb48a7e7
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-03 09:25:14 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - Function To Find IDX of Char
+ * @s: The String To Search In
+ * @c: The Char We Search For
+ *
+ * Return: A Pointer or NULL
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+
+	return (NULL);
+}
