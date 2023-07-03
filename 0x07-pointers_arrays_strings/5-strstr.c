d47e47a909a69c99326a572f3286b16198ff2b48
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-03 10:36:13 -0700

    Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - Function
+ * @haystack: Main String
+ * @needle: Sub String
+ *
+ * Return: Pointer
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0')
+		{
+			if (needle[j] == haystack[i])
+			{
+				p = &haystack[i];
+				return (p);
+			}
+			j++
+		}
+		i++;
+	}
+	return (0);
+}
