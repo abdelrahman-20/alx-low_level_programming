f4e5fd590b3d1e942615e65c94fc8dcf94205244
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-05 13:52:27 -0700

    Task 6 Fix 0

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,22 +3,29 @@
 /**
  * is_prime_number - A Function To Check For Prime Number
  * @n: The Number To Check
- *
+ * @root: Root Number
  * Return: 0 or 1
 */
 
+int check_prime(int n, int root);
 int is_prime_number(int n)
 {
-	int i, prime = 1;
+	return (check_prime(n, 2));
+}
 
-	for (i = 2; i < n; i++)
-	{
-		if (n % i == 0)
-			prime = 0;
-	}
+/**
+ * check_prime - Function To Check Prime Number
+ * @n: The Number
+ * @root: Potential Root
+ * Return: 0 or 1
+*/
 
-	if (prime)
+int check_prime(int n, int root)
+{
+	if (root >= n && n >= 2)
 		return (1);
-	else
+	else if (n % root == 0 || n <= 1)
 		return (0);
+	else
+		return (check_prime(n, root + 1));
 }
