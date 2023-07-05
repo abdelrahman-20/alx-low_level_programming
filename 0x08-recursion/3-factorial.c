3709f1dea327c11e65a9f194b00e0a279baa34ef
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-05 13:11:07 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - A Function To Get The Factorial Of Number
+ * @n: The Number We Get It's Factorial
+ *
+ * Return: The Factorial Result of The Number
+*/
+
+int factorial(int n)
+{
+	if (n <= 1)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
