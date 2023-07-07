6335e51606a52e0e14ab4e6398d5013e9b991c1c
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-07 09:10:13 -0700

    Task 0 Fix 1

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,3 +1,16 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-/***/
+/**
+ * _atoi - Function
+ * @s: The String
+ * Return: integer
+*/
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
