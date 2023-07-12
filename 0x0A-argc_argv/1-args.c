ea082c55bf0659c5b068aae7e67c88a9dd6b1226
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-12 14:27:02 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Main Function
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
