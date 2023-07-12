a1464b5e7d70da94a5a4f5d498a0b36f4b6a6c38
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-11 17:03:16 -0700

    Task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * alloc_grid - Function
+ * @width: Width of 2D Array
+ * @height: Height of 2D Array
+ * Return: 2D Array of Size width * Height
+*/
+
+int **alloc_grid(int width, int height);
+{
+	int i, j, **tab;
+
+	tab = malloc(sizeof(*tab) * height);
+
+	if (width <= 0 || height <= 0 || tab == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			tab[i] = malloc(sizeof(**tab) * width);
+			if (tab[i] == 0)
+			{
+				while (i--)
+					free(tab[i]);
+				free(tab);
+				return (NULL);
+			}
+
+			for (j = 0; j < width; j++)
+				tab[i][j] = 0;
+		}
+	}
+	return (tab);
+}
