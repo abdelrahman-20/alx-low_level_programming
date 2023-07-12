8b96db49537732b08d74da985657e722ca18928d
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-12 14:10:14 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Main Function
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
