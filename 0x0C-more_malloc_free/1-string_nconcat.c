6b50bf80eb7378ca1c29ce354347b29a2945af17
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-13 16:17:35 -0700

    Task 1 Fix 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * string_nconcat - Concating Function
@@ -27,11 +26,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{}
 
 	str = malloc(len1 + n + 1);
+
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; s1[i] != '\0'; i++)
+	{
 		str[i] = s1[i];
+	}
 
 	for (j = 0; j < n; j++)
 	{
