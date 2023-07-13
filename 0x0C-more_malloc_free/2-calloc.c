7342e9d9e745f7db771744b18976dc1a4c722d0b
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-13 16:36:59 -0700

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _memset - Setting Function
+ * @s: ...
+ * @b: ...
+ * @n: ...
+ * Return: Pointer
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+		*s++ = b;
+
+	return (ptr);
+}
+
+/**
+ * _calloc - Function
+ * @nmemb: ...
+ * @size: ....
+ * Return: ...
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *m;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	m = malloc(sizeof(int) * nmemb);
+	if (m == 0)
+		return (NULL);
+
+	_memset(m, 0, sizeof(int) * nmemb);
+}
