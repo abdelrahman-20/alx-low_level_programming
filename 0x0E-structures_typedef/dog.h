bb70300ccd61766580e7ddb94dc5b93dca9b1799
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-17 17:30:19 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - A Dog Struct
+ * @name: ...
+ * @age: ....
+ * @owner: ..
+ *
+ * Describtion: ...
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
