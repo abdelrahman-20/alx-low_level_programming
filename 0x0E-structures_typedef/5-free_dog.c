a43b7ed148baf13c51fb753f8ae07e9bb1a036b5
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-17 18:15:08 -0700

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * free_dog - Function To Free
+ * @d: Struct Type Parameter
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
