39e4192fe2e4a4ea66b0f7aa728f89f393ddc5de
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-20 13:43:41 -0700

    Task 1 Fix 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,8 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		int x = va_arg(args, int);
 
-		printf("%d%s", x, (separator ? separator : ""));
+		printf("%d%s", x, i ? (separator ? separator : "") : "\n");
 	}
-	printf("\n");
 	va_end(args);
 }
