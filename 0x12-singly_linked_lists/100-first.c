351765a456b64ec30ee0b7c0d80a52f3986c78c5
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-07-27 19:11:09 -0700

    Task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void _constructor(void) __attribute__ ((constructor));
+
+/**
+ * _constructor - A Function Executed Before Main Function
+ * Return: Void
+*/
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
+}
