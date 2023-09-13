5baa3297a67d7d39b1d4283a35e9b1b0fc431174
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-09-13 16:29:22 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,13 +9,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	int *end = array + size - 1;
 
 	if (array && size && action)
 	{
-		for (i = 0; i < size; i++)
+		while (array <= end)
 		{
-			action(array[i]);
+			action(array++);
 		}
 	}
 }
