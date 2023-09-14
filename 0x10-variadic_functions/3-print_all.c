6b8b74aab0c1faa483095ea58ccca7a832a9241d
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-09-14 15:12:30 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,90 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
- */
-void print_all(const char * const format, ...)
+ * format_char - A Function
+ * @separator: The Separator
+ * @list: The Parameter To Print
+*/
+
+void format_char(char *separator, va_list list)
 {
-	int i = 0;
-	char *str, *sep = "";
+	printf("%s%c", separator, va_arg(list, int));
+}
 
-	va_list list;
+/**
+ * format_int - A Function
+ * @separator: The Separator
+ * @list: The Parameter To Print
+*/
 
-	va_start(list, format);
+void format_int(char *separator, va_list list)
+{
+	printf("%s%d", separator, va_arg(list, int));
+}
 
-	if (format)
+/**
+ * format_float - A Function
+ * @separator: The Separator
+ * @list: The Parameter To Print
+*/
+
+void format_float(char *separator, va_list list)
+{
+	printf("%s%f", separator, va_arg(list, double));
+}
+
+/**
+ * format_string - A Function
+ * @separator: The Separator
+ * @list: The Parameter To Print
+*/
+
+void format_string(char *separator, va_list list)
+{
+	char *str = va_arg(list, char *);
+
+	switch ((int)(!str))
 	{
-		while (format[i])
-		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%s%c", sep, va_arg(list, int));
-					break;
-				case 'i':
-					printf("%s%d", sep, va_arg(list, int));
-					break;
-				case 'f':
-					printf("%s%f", sep, va_arg(list, double));
-					break;
-				case 's':
-					str = va_arg(list, char *);
-					if (!str)
-						str = "(nil)";
-					printf("%s%s", sep, str);
-					break;
-				default:
-					i++;
-					continue;
-			}
-			sep = ", ";
-			i++;
-		}
+		case 1:
+			str = "(nil)";
 	}
 
+	printf("%s%s", separator, str);
+}
+
+/**
+ * print_all - A General Function To Print
+ * @format: The Foramt of Printed String
+*/
+
+void print_all(const char * const format, ...)
+{
+	int i = 0, j = 0;
+	char *sep = "";
+	va_list list;
+
+	token_t tokens[] = {
+		{"c", format_char},
+		{"i", format_int},
+		{"f", format_float},
+		{"s", format_string},
+		{NULL, NULL}
+	};
+
+	va_start(list, format);
+	while (format && format[i])
+	{
+		while (tokens[j].token)
+		{
+			if (format[i] == tokens[j].token[0])
+			{
+				tokens[j].f(separator, list);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
 	printf("\n");
 	va_end(list);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef _VARIADIC_H_
 #define _VARIADIC_H_
 
+/**
+ * struct token - A Token Struct
+ * @token: The Format Token
+ * @f: The Related Function
+*/
+
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+} token_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
