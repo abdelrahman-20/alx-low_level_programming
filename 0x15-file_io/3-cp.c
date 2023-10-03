d8364cce0d02d3cf4df4a9c73b37b2722e9a9aaf
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-10-02 18:47:33 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 #define USAGE "Usage: cp file_from file_to\n"
 #define ERR_NOREAD "Error: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
