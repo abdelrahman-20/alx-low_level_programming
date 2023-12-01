a20fc603def3191e4ce06764fc285a301d5308d7
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-12-01 11:10:16 -0800

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,9 +10,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned long int i;
 
-	if (size <= 0)
-		return (NULL);
-
 	new_table = malloc(sizeof(hash_table_t) * size);
 	if (!new_table)
 		return (NULL);
