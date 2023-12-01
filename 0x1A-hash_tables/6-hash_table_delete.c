233f4044230b198a02f9b96d1181ae1c2f4fe1d9
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-12-01 12:47:05 -0800

    Task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,7 +11,7 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *node, *tmp;
 
-	if (!ht || ht->array)
+	if (!ht || !ht->array || ht->size == 0)
 		return;
 
 	for (i = 0; i < ht->size; i++)
