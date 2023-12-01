c5bf5add00e66d3f01b2b003e38abc6bac66400f
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-12-01 11:13:51 -0800

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned long int i;
 
-	new_table = malloc(sizeof(hash_table_t) * size);
+	if (size <= 0)
+		return (NULL);
+
+	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 		return (NULL);
 
