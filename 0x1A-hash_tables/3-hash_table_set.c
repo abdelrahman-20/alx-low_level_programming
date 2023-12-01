b1cf2e717278209a5a56b37d6a62f861db42df14
abdelrahman-20 <abdo.alamear25@gmail.com>
2023-12-01 11:32:11 -0800

    Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - A Function To Set A Node in Hash Table
+ * @ht: The Hash Table Structure
+ * @key: The Key of The Node
+ * @value: The Value of The Node
+ *
+ * Return: 1 for Success , 0 Otherwise
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+	hash_node_t *current;
+
+	if (!ht || !key || !value)
+		return (0);
+	index = hash_djb2((unsigned char *)key) % ht->size;
+	current = ht->array[index];
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (!current->value)
+				return (0);
+			return (1);
+		}
+		current = current->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
